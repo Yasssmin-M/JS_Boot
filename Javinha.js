c97b272d0dcb84a4f9b4553baa146b99f90e9734
Yasssmin-M <mpaula.yasmin@gmail.com>
2022-07-06 20:44:44 -0300

    Publicação do site

---
+++ Javinha.js
@@ -0,0 +1,23 @@
+        function album1(){
+			document.getElementById("bf"). src="img/Blurryface.png";
+		}
+		function musica1(){
+			document.getElementById("bf"). src="img/BF.png";
+		}
+
+
+		function album2(){
+			document.getElementById("tc"). src="img/Trench.jpg";
+		}
+		function musica2(){
+			document.getElementById("tc"). src="img/TC.png";
+		}
+
+
+		function album3(){
+			document.getElementById("si"). src="img/Icy.png";
+		}
+		function musica3(){
+			document.getElementById("si"). src="img/SI.png";
+		}
+		
